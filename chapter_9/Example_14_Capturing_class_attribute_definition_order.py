9088a99e86351c2c101b85d703712008c33b89b7
frankie.fu <582547045@qq.com>
2022-06-21 10:28:45 +0800

    Meta programming Example_14

--- chapter_9/Example_14_Capturing_class_attribute_definition_order.py
+++ chapter_9/Example_14_Capturing_class_attribute_definition_order.py
@@ -8,6 +8,27 @@ Solution
 Solution
     Capturing information about the body of class definition is easily accomplished through the use of a
     metaclass.(eg, use an OrderedDict to capture definition order of descriptors)
+Discussion
+    1. The entire key to this recipe is the __prepare__() method, which is defined in the OrderedMeta metaclass.
+    This method is invoked immediately at the start of a class definition with the class name and base classes.
+    It must then return a mapping object use when processing the class body. By returning an OrderedDict instead
+    of a normal dictionary, the resulting definition order is easily captured.
+
+    2. A final important part of this recipe concerns the treatment of the modified dictionary in the metaclass
+    __new__() method. Even though the class was defined using an alternative dictionary, you still have to
+    convert this dictionary to a proper dict instance when making the final class object. This is the purpose
+    of the 'd = dict(clsdict)' statement.
+
+    3. Being able to capture definition order is a subtle but important feature for certain kinds of applications.
+    For instance, in an object relational mapper, classes might be written in a manner to shown in the example:
+        class Stock(Model):
+            name = String()
+            shares = Integer()
+            price = Float()
+    Underneath the covers, the code might want to capture the definition order to map objects to tuples or
+    rows in database table(eg, as_csv method). The solution shown is very straightforward and often simpler
+    than alternative approaches(which typically involve maintaining hidden counters within the descriptor classes).
+
 """
 
 __author__ = 'Frankie Fu'
@@ -96,5 +117,43 @@ print(s.as_csv())
 
 # t = Stock('AAPL', 'a log', 610.23)
 # TypeError: Expected <class 'int'>
+
+
+# Discussion
+# It is possible to extend this functionality even further if you willing to make your own dictionary-like objects.
+# For example, consider this variant of the solution that rejects duplicate definitions:
+
+class NoDupOrderedDict(OrderedDict):
+    def __init__(self, clsname):
+        self.clsname = clsname
+        super().__init__()
+
+    def __setitem__(self, name, value):
+        if name in self:
+            raise TypeError('{} already defined in {}'.format(name, value))
+        super().__setitem__(name, value)
+
+
+class OrderedMeta2(type):
+    def __new__(cls, clsname, bases, clsdict):
+        d = dict(clsdict)
+        d['_order'] = [name for name in clsdict if name[0] != '_']
+        return type.__new__(cls, clsname, bases, d)
+
+    @classmethod
+    def __prepare__(metacls, clsname, bases):
+        return NoDupOrderedDict(clsname)
+
+
+# Here's what happens if you use this metaclass and make a class with duplicate entries:
+class A(metaclass=OrderedMeta2):
+    def spam(self):
+        pass
+
+    # TypeError: spam already defined in <function A.spam at 0x0000023804A05430>
+    # def spam(self):
+    #     pass
+
+
 
 
